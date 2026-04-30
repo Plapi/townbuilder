e4b8fa6dafb464f31c3dd2e1742817fc2f4262a8
Adrian Plapamaru <adrian.plapamaru@intenselife.ch>
2026-04-30 11:12:03 +0300

    fix Simulation Delta Time

--- Assets/Scripts/Factory/SimulationDeltaTime.cs
+++ Assets/Scripts/Factory/SimulationDeltaTime.cs
@@ -3,16 +3,10 @@ using UnityEngine;
 internal class SimulationDeltaTime : Singleton<SimulationDeltaTime>
 {
     public bool IsPaused { get; private set; }
-    public float DeltaTime { get; private set; }
-
-    protected override void Initialize()
-    {
-        DeltaTime = Time.deltaTime;
-    }
+    public float DeltaTime => IsPaused ? 0f : Time.deltaTime;
 
     public void SetPaused(bool paused)
     {
         IsPaused = paused;
-        DeltaTime = paused ? 0f : Time.deltaTime;
     }
 }
