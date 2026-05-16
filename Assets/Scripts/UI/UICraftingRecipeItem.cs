455492b96ffb0a30b2ecce84820d7bb9c1c95213
Adrian Plapamaru <adrian.plapamaru@intenselife.ch>
2026-05-16 19:35:42 +0300

    small update to UICraftingRecipeItem

--- Assets/Scripts/UI/UICraftingRecipeItem.cs
+++ Assets/Scripts/UI/UICraftingRecipeItem.cs
@@ -7,10 +7,17 @@ namespace com.Plapamaru.TownCrafter.UI
     public class UICraftingRecipeItem : MonoBehaviour
     {
         [SerializeField] private List<UIResourceItem> _inputResourceItems;
+        [SerializeField] private GameObject _plusSign;
         [SerializeField] private UIResourceItem _outputResourceItem;
 
         public void Init(CrafterRecipeDefinition recipe)
         {
+            for (var i = 0; i < _inputResourceItems.Count; i++)
+                _inputResourceItems[i].gameObject.SetActive(i < recipe.inputs.Count);
+
+            if (_plusSign != null)
+                _plusSign.SetActive(recipe.inputs.Count > 1);
+
             for (var i = 0; i < recipe.inputs.Count && i < _inputResourceItems.Count; i++)
                 _inputResourceItems[i].Init(recipe.inputs[i].resourceItem, recipe.inputs[i].amount);
 
